9c5be32d41c053a0725061d257beddf363e4f6c9
Harish-Karthik <harishkarthik23579@gmail.com>
2025-05-07 13:42:36 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+
+#define LED_1 GPIO_NUM_2
+
+void app_main(void)
+{
+    esp_rom_gpio_pad_select_gpio(LED_1);
+    gpio_set_direction(LED_1, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(LED_1, 1);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        gpio_set_level(LED_1, 0);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
